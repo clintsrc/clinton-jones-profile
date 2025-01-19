ff8c6d28a5e33cdd9d1e8c01fdda9a58c6280bf5
Clint Jones <clinton.alan.jones@gmail.com>
2025-01-19 15:36:41 -0800

    Making the portfolio maintainable

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -19,69 +19,56 @@ import imgNerdTrivia from "../assets/images/app-NerdTrivia.jpg";
 // import imgReadmeGen from "../assets/images/app-ReadmeGen.jpg";
 
 function Portfolio() {
+  
+  const projects = [
+    {
+      imagetile: imgNerdTrivia,
+    },
+    {
+      imagetile: imgNerdTrivia,
+    },
+  ];
+
   return (
     <div className="container mt-5">
       <div className="row g-4">
-        <div className="col-12 col-sm-6 col-md-4">
-          <div className="tile">
-            <img src={imgNerdTrivia} alt="Nerd Trivia" className="img-fluid" />
-            <div className="overlay">
-              <div className="content">
-                <h3>
-                  <a
-                    href="https://clintsrc.github.io/nerd-trivia/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="portfolio-link"
-                  >
-                    Nerd Trivia
-                  </a>
-                  <a
-                    href="https://github.com/clintsrc/nerd-trivia"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="portfolio-link"
-                  >
-                    <i className="bi bi-github"></i>
-                  </a>
-                </h3>
-                <div>HTML</div>
-              </div>
-            </div>
-          </div>
-        </div>
 
-        <div className="col-12 col-sm-6 col-md-4">
-          <div className="tile">
-            <img src={imgNerdTrivia} alt="Nerd Trivia" className="img-fluid" />
-            <div className="overlay">
-              <div className="content">
-                <h3>
-                  <a
-                    href="https://clintsrc.github.io/nerd-trivia/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="portfolio-link"
-                  >
-                    Nerd Trivia
-                  </a>
-                  <a
-                    href="https://github.com/clintsrc/nerd-trivia"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="portfolio-link"
-                  >
-                    <i className="bi bi-github"></i>
-                  </a>
-                </h3>
-                <div>HTML</div>
+
+        {projects.map((project, index) => (
+          <div className="col-12 col-sm-6 col-md-4" key={index}>
+            <div className="tile">
+              <img src={project.imagetile} alt="Nerd Trivia" className="img-fluid" />
+              <div className="overlay">
+                <div className="content">
+                  <h3>
+                    <a
+                      href="https://clintsrc.github.io/nerd-trivia/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="portfolio-link"
+                    >
+                      Nerd Trivia
+                    </a>
+                    <a
+                      href="https://github.com/clintsrc/nerd-trivia"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="portfolio-link"
+                    >
+                      <i className="bi bi-github"></i>
+                    </a>
+                  </h3>
+                  <div>HTML</div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
+
+        
       </div>
     </div>
-  );
+  );  // end jsx return
 }
 
 export default Portfolio;
